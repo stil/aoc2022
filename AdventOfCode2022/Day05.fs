14699bc8167353ca97b7ec44689f1d969f4168e1
stil <p@ryciuk.com>
2022-12-05 19:45:36 +0100

    Shave day 5

--- AdventOfCode2022/Day05.fs
+++ AdventOfCode2022/Day05.fs
@@ -3,29 +3,26 @@ module Day05
 let partial = Helpers.readInputFull 5
 let split (delim: string) (str: string) = str.Split(delim)
 let replace (oldValue: string) (newValue: string) (str: string) = str.Replace(oldValue, newValue)
-let parts = partial |> split "\r\n\r\n" |> Seq.map (split "\r\n") |> Seq.toList
-let concat (chars: char[]) = System.String.Concat(chars)
-
-let stackInput = parts[0]
+let input = partial |> split "\r\n\r\n" |> Seq.map (split "\r\n") |> Seq.toList
+let concat (chars: char seq) = System.String.Concat(chars)
 
 let initialCrateStacks =
     [ 0..8 ]
     |> Seq.map (fun col ->
         [ 0..7 ]
-        |> Seq.map (fun row -> stackInput[row][col * 4 + 1])
+        |> Seq.map (fun row -> (input[0][row])[col * 4 + 1])
         |> Seq.filter (fun c -> c <> ' ')
         |> Seq.toList)
     |> Seq.toList
 
-
 let instructions =
-    parts[1]
+    input[1]
     |> Seq.map (replace "move " "" >> replace " to " " " >> replace " from " " ")
-    |> Seq.map (split " " >> Seq.map int >> Seq.toList)
+    |> Seq.map (split " " >> Array.map int)
     |> Seq.map (fun parts -> (parts[0], parts[1], parts[2]))
     |> Seq.toList
 
-let crateStacks collectFn =
+let topCrates collectFn =
     instructions
     |> Seq.collect collectFn
     |> Seq.fold
@@ -38,13 +35,10 @@ let crateStacks collectFn =
             |> List.updateAt (moveTo - 1) newTargetStack
             |> List.updateAt (moveFrom - 1) newSourceStack)
         initialCrateStacks
-    |> Seq.map (fun stack -> stack |> Seq.head)
-    |> Seq.toArray
+    |> Seq.map Seq.head
 
 let part1 =
-    let collectFn =
-        (fun (howMany, moveFrom, moveTo) -> [ 1..howMany ] |> Seq.map (fun _ -> (1, moveFrom, moveTo)))
+    topCrates (fun (howMany, moveFrom, moveTo) -> Seq.init howMany (fun _ -> (1, moveFrom, moveTo)))
+    |> concat
 
-    crateStacks collectFn |> concat
-
-let part2 = crateStacks (fun instr -> [ instr ]) |> concat
+let part2 = topCrates (fun instr -> [ instr ]) |> concat
