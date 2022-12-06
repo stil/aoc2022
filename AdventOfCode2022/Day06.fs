09922fd58466b1440e617f3abde73aead54e91a3
stil <p@ryciuk.com>
2022-12-06 11:30:05 +0100

    Shave day 6

--- AdventOfCode2022/Day06.fs
+++ AdventOfCode2022/Day06.fs
@@ -1,16 +1,10 @@
 module Day06
 
-let input = Helpers.readInput 6 |> Seq.head
+let input = Helpers.readInput 6 |> List.head
 
 let markerPosition markerLength =
-    let partial =
-        input
-        |> Seq.mapi (fun i char -> (i, char))
-        |> Seq.map (fun (i, _) -> (i, Seq.init markerLength (fun off -> input[off + i])))
-        |> Seq.filter (fun (pos, coll) -> coll |> Seq.distinct |> Seq.length = markerLength)
-        |> Seq.head
-
-    fst partial + markerLength
+    [ markerLength + 1 .. input.Length - 1 ]
+    |> Seq.find (fun i -> Set input[i - markerLength .. i - 1] |> Set.count = markerLength)
 
 let part1 = markerPosition 4
 let part2 = markerPosition 14
