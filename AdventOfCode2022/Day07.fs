38c6f1fcf648edd36a56552ded8bf36af2ef37aa
stil <p@ryciuk.com>
2022-12-07 18:39:12 +0100

    Cleanup day 7

--- AdventOfCode2022/Day07.fs
+++ AdventOfCode2022/Day07.fs
@@ -12,103 +12,98 @@ and FileInfo =
 and DirectoryInfo =
     { name: string
       mutable subitems: FileSystemItem list
-      mutable parent: DirectoryInfo option }
+      parent: DirectoryInfo option }
 
-let input = Helpers.readInput 7
+let fileSystem =
 
-let isCommand (line: string) = line.StartsWith('$')
+    let input = Helpers.readInput 7
 
-let inputParsed =
-    input
-    |> Seq.mapi (fun i line -> (i, line))
-    |> Seq.filter (fun (i, line) -> isCommand line)
-    |> Seq.map (fun (i, line) ->
-        (line,
-         input
-         |> Seq.skip (i + 1)
-         |> Seq.takeWhile (fun line -> line |> isCommand |> not)
-         |> Seq.toList))
+    let isCommand (line: string) = line.StartsWith('$')
 
-let root =
-    { name = "/"
-      subitems = []
-      parent = None }
+    let inputParsed =
+        input
+        |> Seq.mapi (fun i line -> (i, line))
+        |> Seq.filter (fun (i, line) -> isCommand line)
+        |> Seq.map (fun (i, line) ->
+            (line,
+             input
+             |> Seq.skip (i + 1)
+             |> Seq.takeWhile (fun line -> line |> isCommand |> not)
+             |> Seq.toList))
 
+    let root =
+        { name = "/"
+          subitems = []
+          parent = None }
 
-let mutable cwd = root
+    let mutable cwd = root
 
+    let cd (destination: string) =
+        printfn $"cd to %s{destination}"
 
-let cd (destination: string) =
-    printfn $"cd to %s{destination}"
+        if destination = ".." then
+            cwd <- cwd.parent.Value
+        else
+            let items =
+                cwd.subitems
+                |> Seq.filter (fun item ->
+                    match item with
+                    | Directory dirinfo -> dirinfo.name = destination
+                    | _ -> false)
 
-    if destination = ".." then
-        cwd <- cwd.parent.Value
-    else
-        let items =
-            cwd.subitems
-            |> Seq.filter (fun item ->
-                match item with
-                | Directory (dirinfo) -> dirinfo.name = destination
-                | _ -> false)
+            let item = items |> Seq.head
 
-        let item = items |> Seq.head
-
-        match item with
-        | Directory (dirinfo) -> cwd <- dirinfo
-
-
-
-let ls (output: string list) =
-    printfn "ls"
-
-    let entries =
-        output
-        |> Seq.map (fun dirent ->
-            let parts = dirent.Split(' ')
-
-            match parts[0] with
-            | "dir" ->
-                Directory
-                    { name = parts[1]
-                      subitems = []
-                      parent = Some(cwd) }
-            | _ ->
-                File
-                    { name = parts[1]
-                      fileSize = uint64 parts[0]
-                      parent = Some(cwd) })
-        |> Seq.toList
+            match item with
+            | Directory dirinfo -> cwd <- dirinfo
 
 
-    cwd.subitems <- entries
-    ()
 
-let consumeCommand (line: string) (output: string list) =
-    let parts = line.Split(' ')
+    let ls (output: string list) =
+        printfn "ls"
 
-    match parts[1] with
-    | "cd" -> cd parts[2]
-    | "ls" -> ls output
-    | _ -> failwith "Unsupported command."
+        let entries =
+            output
+            |> Seq.map (fun dirent ->
+                let parts = dirent.Split(' ')
 
-inputParsed |> Seq.skip 1 |> Seq.iter (fun cmd -> cmd ||> consumeCommand)
+                match parts[0] with
+                | "dir" ->
+                    Directory
+                        { name = parts[1]
+                          subitems = []
+                          parent = Some(cwd) }
+                | _ ->
+                    File
+                        { name = parts[1]
+                          fileSize = uint64 parts[0]
+                          parent = Some(cwd) })
+            |> Seq.toList
 
-let rec traverse (current: FileSystemItem) (itemFn: FileSystemItem -> unit) =
-    match current with
-    | File _ -> itemFn current
-    | Directory dir ->
-        itemFn current
-        dir.subitems |> Seq.iter (fun cmd -> traverse cmd itemFn)
 
-let descendants (node: DirectoryInfo) =
-    let mutable result: FileSystemItem list = []
-    traverse (Directory node) (fun item -> result <- result |> List.append [ item ])
-    result
+        cwd.subitems <- entries
+        ()
+
+    let consumeCommand (line: string) (output: string list) =
+        let parts = line.Split(' ')
+
+        match parts[1] with
+        | "cd" -> cd parts[2]
+        | "ls" -> ls output
+        | _ -> failwith "Unsupported command."
+
+    inputParsed |> Seq.skip 1 |> Seq.iter (fun cmd -> cmd ||> consumeCommand)
+
+    Directory root
+
+let rec descendants (node: FileSystemItem) =
+    match node with
+    | File _ -> [ node ]
+    | Directory dir -> (Directory dir) :: (dir.subitems |> List.collect descendants)
 
 let sumSize (node: FileSystemItem) =
     match node with
-    | Directory (dir) ->
-        descendants dir
+    | Directory dir ->
+        descendants (Directory dir)
         |> Seq.choose (fun item ->
             match item with
             | File file -> Some(file)
@@ -117,7 +112,7 @@ let sumSize (node: FileSystemItem) =
     | File file -> file.fileSize
 
 let directorySizes =
-    descendants root
+    descendants fileSystem
     |> Seq.choose (fun item ->
         match item with
         | Directory info -> Some(info)
@@ -129,7 +124,7 @@ let directorySizes =
 let part1 = directorySizes |> Seq.filter (fun size -> size <= 100000UL) |> Seq.sum
 
 let part2 =
-    let usedSpace = sumSize (Directory root)
+    let usedSpace = sumSize fileSystem
 
     directorySizes
     |> Seq.sort
