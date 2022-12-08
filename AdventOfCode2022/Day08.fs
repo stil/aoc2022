c785ccacbfe201d5f41ca13d66224e0938d892a4
stil <p@ryciuk.com>
2022-12-08 23:07:22 +0100

    Cleanup day 8

--- AdventOfCode2022/Day08.fs
+++ AdventOfCode2022/Day08.fs
@@ -5,7 +5,7 @@ let input = Helpers.readInput 8
 let grid =
     Array2D.init input.Length input[0].Length (fun y x -> int (input[y][x] - '0'))
 
-let getHeight = Array2D.get grid
+let getTreeHeight = Array2D.get grid
 let height = Array2D.length1 grid
 let width = Array2D.length2 grid
 
@@ -25,16 +25,14 @@ let part1 =
     let rays = [ down; up; right; left ] |> Seq.concat
 
     let visibleTrees ray =
-        let maxHeightSoFar, tallerCount =
-            ray
-            |> Seq.fold
-                (fun (maxHeightSoFar, tallerList) (y, x) ->
-                    let height = getHeight y x
-                    let taller = height > maxHeightSoFar
-                    (max height maxHeightSoFar, (if taller then (y, x) :: tallerList else tallerList)))
-                (-1, [])
-
-        tallerCount
+        ray
+        |> Seq.fold
+            (fun (maxHeightSoFar, tallerList) (y, x) ->
+                let height = getTreeHeight y x
+                let taller = height > maxHeightSoFar
+                (max height maxHeightSoFar, (if taller then (y, x) :: tallerList else tallerList)))
+            (-1, [])
+        |> snd
 
     rays |> Seq.map visibleTrees |> Seq.collect id |> Set |> Set.count
 
@@ -47,7 +45,7 @@ let part2 =
                 ray
                 |> Seq.fold
                     (fun (tallerList, skip) (y, x) ->
-                        let height = getHeight y x
+                        let height = getTreeHeight y x
                         let blocked = height >= treeHeight
 
                         ((if skip then tallerList else (y, x) :: tallerList), (if skip then true else blocked)))
@@ -63,7 +61,7 @@ let part2 =
         let up = range 0 houseY |> Seq.map (fun y -> (houseY - 1 - y, houseX))
         let left = range 0 houseX |> Seq.map (fun x -> (houseY, houseX - 1 - x))
 
-        let currentHeight = getHeight houseY houseX
+        let currentHeight = getTreeHeight houseY houseX
 
         [ up; down; left; right ]
         |> Seq.map (Seq.toList >> viewingDistance currentHeight)
