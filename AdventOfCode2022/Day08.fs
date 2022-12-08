0e7ff1edb1c74fd6ce8529476f9ffc5a04550869
stil <p@ryciuk.com>
2022-12-09 00:39:35 +0100

    Use path walking in day 8

--- AdventOfCode2022/Day08.fs
+++ AdventOfCode2022/Day08.fs
@@ -8,59 +8,58 @@ let width = input[0].Length
 
 let range minIncl maxExcl = { minIncl .. (maxExcl - 1) }
 
+let outOfBounds y x =
+    y < 0 || x < 0 || y >= height || x >= width
+
+let walkPath (dirY, dirX) y0 x0 =
+    (y0, x0)
+    |> Seq.unfold (fun (y, x) ->
+        if outOfBounds y x then
+            None
+        else
+            Some((y, x), (y + dirY, x + dirX)))
+
+let walkDown = walkPath (1, 0)
+let walkRight = walkPath (0, 1)
+let walkUp = walkPath (-1, 0)
+let walkLeft = walkPath (0, -1)
+
 let part1 =
-    let down =
-        range 0 width |> Seq.map (fun x -> range 0 height |> Seq.map (fun y -> (y, x)))
+    let visibleTrees ray =
+        ray
+        |> Seq.fold
+            (fun (tallerList, maxHeightSoFar) (y, x) ->
+                let height = getTreeHeight y x
+                let taller = height > maxHeightSoFar
+                ((if taller then (y, x) :: tallerList else tallerList), max height maxHeightSoFar))
+            ([], -1)
+        |> fst
 
-    let right =
-        range 0 height |> Seq.map (fun y -> range 0 width |> Seq.map (fun x -> (y, x)))
-
+    let down = range 0 width |> Seq.map (fun x -> (0, x) ||> walkDown)
+    let right = range 0 height |> Seq.map (fun y -> (y, 0) ||> walkRight)
     let up = down |> Seq.map Seq.rev
     let left = right |> Seq.map Seq.rev
 
     let rays = [ down; up; right; left ] |> Seq.concat
-
-    let visibleTrees ray =
-        ray
-        |> Seq.fold
-            (fun (maxHeightSoFar, tallerList) (y, x) ->
-                let height = getTreeHeight y x
-                let taller = height > maxHeightSoFar
-                (max height maxHeightSoFar, (if taller then (y, x) :: tallerList else tallerList)))
-            (-1, [])
-        |> snd
-
     rays |> Seq.map visibleTrees |> Seq.collect id |> Set |> Set.count
 
 let part2 =
-    let viewingDistance treeHeight (ray: (int * int) list) =
-        if ray.Length = 0 then
-            0
-        else
-            ray
-            |> Seq.fold
-                (fun (tallerList, skip) (y, x) ->
-                    let height = getTreeHeight y x
-                    let blocked = height >= treeHeight
-
-                    ((if skip then tallerList else (y, x) :: tallerList), (if skip then true else blocked)))
-                ([], false)
-            |> fst
-            |> Seq.length
-
+    let viewingDistance treeHeight ray =
+        ray
+        |> Seq.fold
+            (fun (tallerCount, skip) (y, x) ->
+                let height = getTreeHeight y x
+                let blocked = height >= treeHeight
+                ((if skip then tallerCount else tallerCount + 1), (if skip then true else blocked)))
+            (0, false)
+        |> fst
 
     (range 0 height, range 0 width)
     ||> Seq.allPairs
     |> Seq.map (fun (houseY, houseX) ->
-        let down = range (houseY + 1) height |> Seq.map (fun y -> (y, houseX))
-        let right = range (houseX + 1) width |> Seq.map (fun x -> (houseY, x))
-        let up = range 0 houseY |> Seq.map (fun y -> (houseY - 1 - y, houseX))
-        let left = range 0 houseX |> Seq.map (fun x -> (houseY, houseX - 1 - x))
-
-        let currentHeight = getTreeHeight houseY houseX
-
-        [ up; down; left; right ]
-        |> Seq.map (Seq.toList >> viewingDistance currentHeight)
+        [ walkDown; walkRight; walkUp; walkLeft ]
+        |> Seq.map (fun walkFn -> (houseY, houseX) ||> walkFn |> Seq.skip 1)
+        |> Seq.map (Seq.toList >> viewingDistance (getTreeHeight houseY houseX))
         |> Seq.reduce (*))
     |> Seq.max
 
