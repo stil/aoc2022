4d64fb807dcfb0648f76c8f6c7b6490d263be13b
stil <p@ryciuk.com>
2022-12-09 23:34:06 +0100

    Cleanup day 9

--- AdventOfCode2022/Day09.fs
+++ AdventOfCode2022/Day09.fs
@@ -1,30 +1,30 @@
 module Day09
 
-let step dirY dirX y0 x0 = (y0 + dirY, x0 + dirX)
+let add (y0, x0) (y1, x1) = (y0 + y1, x0 + x1)
+let subtract (y0, x0) (y1, x1) = (y0 - y1, x0 - x1)
 
 let stepFnIntoDirection dirChar =
     match dirChar with
-    | 'R' -> step 0 1
-    | 'L' -> step 0 -1
-    | 'U' -> step 1 0
-    | 'D' -> step -1 0
+    | 'R' -> add (0, 1)
+    | 'L' -> add (0, -1)
+    | 'U' -> add (1, 0)
+    | 'D' -> add (-1, 0)
     | _ -> failwith "Unsupported direction."
 
 let input =
     Helpers.readInput 9
     |> Seq.collect (fun line -> (int line[2..], stepFnIntoDirection line[0]) ||> Seq.replicate)
 
-let distance y0 x0 y1 x1 = (y1 - y0, x1 - x0)
 let isTouching (x, y) = abs x <= 1 && abs y <= 1
 let clamp value = value |> max -1 |> min 1
 
 let pullVector (x, y) =
     if isTouching (x, y) then (0, 0) else (clamp -x, clamp -y)
 
-let findNextTailPos (tailPos: int * int) (nextHeadPos: int * int) =
-    let distance = tailPos ||> (nextHeadPos ||> distance)
+let findNextTailPos (tailPos: int * int) (headPos: int * int) =
+    let distance = subtract tailPos headPos
     let pullVector = pullVector distance
-    (fst tailPos + fst pullVector, snd tailPos + snd pullVector)
+    add tailPos pullVector
 
 let adjustRope rope =
     [ 0 .. (rope |> List.length) - 2 ]
@@ -42,7 +42,7 @@ let countUniqueVisitedByTail ropeLength =
     let finalRope, visitedByTail =
         ((rope, []), input)
         ||> Seq.fold (fun (rope, visited) stepFn ->
-            let newRope = rope |> List.updateAt 0 (rope |> List.head ||> stepFn) |> adjustRope
+            let newRope = rope |> List.updateAt 0 (rope |> List.head |> stepFn) |> adjustRope
             let newRopeTail = newRope |> List.last
             (newRope, newRopeTail :: visited))
 
