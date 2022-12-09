7af1030cf2c1e41b0e2a85561e84610fbd6da404
stil <p@ryciuk.com>
2022-12-09 22:33:01 +0100

    Cleanup day 9

--- AdventOfCode2022/Day09.fs
+++ AdventOfCode2022/Day09.fs
@@ -1,36 +1,30 @@
 module Day09
 
-let step (dirY, dirX) y0 x0 = (y0 + dirY, x0 + dirX)
+let step dirY dirX y0 x0 = (y0 + dirY, x0 + dirX)
+
+let stepFnIntoDirection dirChar =
+    match dirChar with
+    | 'R' -> step 0 1
+    | 'L' -> step 0 -1
+    | 'U' -> step 1 0
+    | 'D' -> step -1 0
+    | _ -> failwith "Unsupported direction."
 
 let input =
     Helpers.readInput 9
-    |> Seq.map (fun line ->
-        (int line[2..],
-         match line[0] with
-         | 'R' -> step (0, 1)
-         | 'L' -> step (0, -1)
-         | 'U' -> step (1, 0)
-         | 'D' -> step (-1, 0)
-         | _ -> failwith "Unsupported direction."))
-    |> Seq.collect (fun instr -> instr ||> Seq.replicate)
+    |> Seq.collect (fun line -> (int line[2..], stepFnIntoDirection line[0]) ||> Seq.replicate)
 
 let distance y0 x0 y1 x1 = (y1 - y0, x1 - x0)
 let isTouching (x, y) = abs x <= 1 && abs y <= 1
+let clamp value = value |> max -1 |> min 1
 
-let clamp value =
-    if value >= 0 then min 1 value else max -1 value
-
-let pullVector (x, y) = (clamp -x, clamp -y)
+let pullVector (x, y) =
+    if isTouching (x, y) then (0, 0) else (clamp -x, clamp -y)
 
 let findNextTailPos (tailPos: int * int) (nextHeadPos: int * int) =
     let distance = tailPos ||> (nextHeadPos ||> distance)
-    let isTouching = isTouching distance
-
-    if not isTouching then
-        let pullVector = pullVector distance
-        (fst tailPos + fst pullVector, snd tailPos + snd pullVector)
-    else
-        tailPos
+    let pullVector = pullVector distance
+    (fst tailPos + fst pullVector, snd tailPos + snd pullVector)
 
 let adjustRope rope =
     [ 0 .. (rope |> List.length) - 2 ]
