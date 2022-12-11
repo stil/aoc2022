428158595905b493e4af047fd10b6d9d25cbf960
stil <p@ryciuk.com>
2022-12-11 23:31:35 +0100

    Cleanup day 11

--- AdventOfCode2022/Day11.fs
+++ AdventOfCode2022/Day11.fs
@@ -1,12 +1,11 @@
 module Day11
 
 // a * divisorsProduct + b
-type SpecialNumber = uint64 * uint64
+type ComplexNumber = uint64 * uint64
 
 type Monkey =
-    { Number: int
-      Items: SpecialNumber list
-      OperationFn: SpecialNumber -> SpecialNumber
+    { Items: ComplexNumber list
+      OperationFn: ComplexNumber -> ComplexNumber
       Divisor: uint64
       MonkeyNumWhenTrue: int
       MonkeyNumWhenFalse: int
@@ -18,26 +17,22 @@ let divisorsProduct =
     |> Seq.map (fun line -> uint64 (line.Replace("  Test: divisible by ", "")))
     |> Seq.reduce (fun a b -> a * b)
 
-let uint64ToSpecialNumber (input: uint64) =
-    let quotient = input / divisorsProduct
+let intToComplexNum (input: uint64) =
+    let quotient = (input / divisorsProduct) |> min 1UL // Reduce.
     let remainder = input % divisorsProduct
-    let quotient = min quotient 1UL // Reduce.
     (quotient, remainder)
 
-let specialNumberToUint64 ((quotient, remainder): SpecialNumber) = quotient * divisorsProduct + remainder
+let complexNumToInt ((quotient, remainder): ComplexNumber) = quotient * divisorsProduct + remainder
+let mapTuple fn (a, b) = (fn a, fn b)
 
 let binaryFunctionOnIntegers left right fn =
-    let result = fn (specialNumberToUint64 left) (specialNumberToUint64 right)
-    uint64ToSpecialNumber result
+    (left, right) |> mapTuple complexNumToInt ||> fn |> intToComplexNum
 
 let add left right = binaryFunctionOnIntegers left right (+)
 let multiply left right = binaryFunctionOnIntegers left right (*)
 
-let divideByInt divident (divisor: uint64) =
-    ((divident |> specialNumberToUint64) / divisor) |> uint64ToSpecialNumber
-
-let divisibilityTest divident (divisor: uint64) =
-    (specialNumberToUint64 divident) % divisor = 0UL
+let divisibilityTest divident divisor =
+    (complexNumToInt divident) % divisor = 0UL
 
 let monkeys =
     (Helpers.readInputFull 11).Split("\r\n\r\n")
@@ -46,23 +41,22 @@ let monkeys =
 
         let operationParts = lines[ 2 ].Replace("  Operation: new = old ", "").Split(' ')
 
-        let operationFn =
+        let partialOperationFn =
             match operationParts[0] with
             | "+" -> add
             | "*" -> multiply
             | _ -> failwith "Unsupported operator."
 
-        let partialOperationFn left =
+        let operationFn left =
             match operationParts[1] with
-            | "old" -> operationFn left left
-            | _ -> operationFn left (uint64ToSpecialNumber (uint64 operationParts[1]))
+            | "old" -> partialOperationFn left left
+            | _ -> partialOperationFn left (intToComplexNum (uint64 operationParts[1]))
 
-        { Number = int (lines[ 0 ].Replace("Monkey ", "").Replace(":", ""))
-          Items =
+        { Items =
             lines[ 1 ].Replace("  Starting items: ", "").Split(", ")
-            |> Seq.map (fun v -> (uint64ToSpecialNumber (uint64 (int v))))
+            |> Seq.map (fun v -> (intToComplexNum (uint64 (int v))))
             |> Seq.toList
-          OperationFn = partialOperationFn
+          OperationFn = operationFn
           Divisor = uint64 (lines[ 3 ].Replace("  Test: divisible by ", ""))
           MonkeyNumWhenTrue = int (lines[ 4 ].Replace("    If true: throw to monkey ", ""))
           MonkeyNumWhenFalse = int (lines[ 5 ].Replace("    If false: throw to monkey ", ""))
@@ -70,8 +64,8 @@ let monkeys =
     |> Seq.toList
 
 let solve roundCount afterInspectionFn =
-    let playSingleItem (monkeyList: Monkey list) itemWorryLevel monkeyNum =
-        let monkey = monkeyList[monkeyNum]
+    let advanceSingleItem (monkeys: Monkey list) monkeyNum itemWorryLevel =
+        let monkey = monkeys[monkeyNum]
         let worryLevelAtInspection = monkey.OperationFn itemWorryLevel
         let worryLevelAfterInspection = afterInspectionFn worryLevelAtInspection
 
@@ -80,7 +74,7 @@ let solve roundCount afterInspectionFn =
             | true -> monkey.MonkeyNumWhenTrue
             | false -> monkey.MonkeyNumWhenFalse
 
-        let targetMonkey = monkeyList[targetMonkeyNum]
+        let targetMonkey = monkeys[targetMonkeyNum]
 
         let updatedTargetMonkey =
             { targetMonkey with Items = ([ worryLevelAfterInspection ] |> List.append targetMonkey.Items) }
@@ -90,27 +84,27 @@ let solve roundCount afterInspectionFn =
                 Items = (monkey.Items |> List.skip 1)
                 InspectionCount = monkey.InspectionCount + 1UL }
 
-        monkeyList
-        |> List.updateAt monkey.Number updatedCurrentMonkey
+        monkeys
+        |> List.updateAt monkeyNum updatedCurrentMonkey
         |> List.updateAt targetMonkeyNum updatedTargetMonkey
 
-    let playRound (monkeys: Monkey list) =
+    let advanceMonkey (monkeys: Monkey list) monkeyNum =
+        monkeys[monkeyNum].Items
+        |> Seq.fold (fun monkeys -> advanceSingleItem monkeys monkeyNum) monkeys
+
+    let advanceRound (monkeys: Monkey list) =
         monkeys
-        |> Seq.map (fun monkey -> monkey.Number)
-        |> Seq.fold
-            (fun (monkeyList: Monkey list) monkeyNum ->
-                monkeyList[monkeyNum].Items
-                |> Seq.fold (fun monkeyList worryLevel -> playSingleItem monkeyList worryLevel monkeyNum) monkeyList)
-            monkeys
+        |> Seq.mapi (fun i _ -> i)
+        |> Seq.fold (fun (monkeys: Monkey list) -> advanceMonkey monkeys) monkeys
 
     Seq.init roundCount id
-    |> Seq.fold (fun monkeyList roundNum -> playRound monkeyList) monkeys
+    |> Seq.fold (fun monkeys roundNum -> advanceRound monkeys) monkeys
     |> Seq.map (fun m -> m.InspectionCount)
     |> Seq.sortByDescending id
     |> Seq.take 2
     |> Seq.reduce (fun a b -> a * b)
 
-let part1 = solve 20 (fun x -> divideByInt x 3UL)
+let part1 = solve 20 (fun x -> ((x |> complexNumToInt) / 3UL) |> intToComplexNum)
 let part2 = solve 10000 id
 
 Helpers.assertEqual 99840UL part1
