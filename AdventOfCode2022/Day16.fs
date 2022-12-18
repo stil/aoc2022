acfbae164413a67440e69a4a946072f9b7419f7c
stil <p@ryciuk.com>
2022-12-18 15:52:40 +0100

    Checkpoint

--- AdventOfCode2022/Day16.fs
+++ AdventOfCode2022/Day16.fs
@@ -2,6 +2,78 @@ module Day16
 
 open System.Collections.Generic
 
+type ValveDestination = { name: string; costInMinutes: int }
+
+type ValveDefinition =
+    { name: string
+      flowRate: int
+      leadsTo: ValveDestination Set }
+
+let rec valveDefinitions () =
+    let definitions =
+        Helpers.readInput 16
+        |> Seq.map (fun line ->
+            let parts =
+                line
+                    .Replace("Valve ", "")
+                    .Replace(" has flow rate=", "\t")
+                    .Replace("tunnels", "tunnel")
+                    .Replace("valves", "valve")
+                    .Replace("leads", "lead")
+                    .Replace("; tunnel lead to valve ", "\t")
+                    .Split("\t")
+
+            { name = parts[0]
+              flowRate = (int parts[1])
+              leadsTo =
+                parts[ 2 ].Split(", ")
+                |> Seq.map (fun destValveName ->
+                    { name = destValveName
+                      costInMinutes = 1 })
+                |> Set })
+        |> Seq.toList
+
+    let valveDefinitionsDict = definitions |> Seq.map (fun vd -> (vd.name, vd)) |> dict
+
+    let getValve valveName = valveDefinitionsDict[valveName]
+
+    let rec flattenDestinations depth (destinations: ValveDestination seq) =
+        if depth = 11 then
+            []
+        else
+            let leadsTo =
+                destinations
+                |> Seq.map (fun dest ->
+                    let destValve = getValve dest.name
+
+                    if destValve.flowRate = 0 then
+                        destValve.leadsTo |> flattenDestinations (depth + 1)
+                    else
+                        [ { dest with costInMinutes = depth } ])
+                |> Seq.collect id
+                |> Seq.toList
+
+            leadsTo
+
+    let flattenValve (valve: ValveDefinition) =
+        { valve with
+            leadsTo =
+                valve.leadsTo
+                |> flattenDestinations 1
+                |> Set
+                |> Seq.sortBy (fun dest -> (dest.name, dest.costInMinutes))
+                |> Seq.distinctBy (fun dest -> dest.name)
+                |> Seq.filter (fun dest -> dest.name <> valve.name)
+                |> Set }
+
+    let result =
+        definitions
+        |> List.map flattenValve
+        |> Seq.filter (fun def -> def.name = "AA" || def.flowRate > 0)
+        |> Seq.toList
+
+
+    result
 
 let aStar neighborsFn heuristicFn costFn (start: 'a) (goalReachedFn: 'a -> bool) (currentFn: 'a -> unit) =
     let frontier = PriorityQueue<'a, int>()
@@ -54,42 +126,20 @@ let aStar neighborsFn heuristicFn costFn (start: 'a) (goalReachedFn: 'a -> bool)
             None
 
 
-type ValveDefinition =
-    { name: string
-      flowRate: int
-      leadsTo: string Set }
-
-type OpenValve =
-    { valve: ValveDefinition
-      openAtMinutesLeft: int }
-
 type Node =
     { minutesLeft: int
       currentlyAtValve: ValveDefinition
-      openValves: OpenValve list
+      openValves: string Set
+      pressureReleased: int
       traveledPath: string list }
 
 let part1 =
-    let valveDefinitions =
-        Helpers.readInput 16
-        |> Seq.map (fun line ->
-            let parts =
-                line
-                    .Replace("Valve ", "")
-                    .Replace(" has flow rate=", "\t")
-                    .Replace("tunnels", "tunnel")
-                    .Replace("valves", "valve")
-                    .Replace("leads", "lead")
-                    .Replace("; tunnel lead to valve ", "\t")
-                    .Split("\t")
+    let valveDefinitions = valveDefinitions ()
 
-            { name = parts[0]
-              flowRate = (int parts[1])
-              leadsTo = parts[ 2 ].Split(", ") |> Set })
-        |> Seq.toList
+    let valveDefinitionsDict =
+        valveDefinitions |> Seq.map (fun vd -> (vd.name, vd)) |> dict
 
-    let getValve valveName =
-        valveDefinitions |> Seq.find (fun valve -> valve.name = valveName)
+    let getValve valveName = valveDefinitionsDict[valveName]
 
     let allValveNames = valveDefinitions |> Seq.map (fun valve -> valve.name) |> Set
 
@@ -100,13 +150,12 @@ let part1 =
         |> Set
 
     let getNeighbors (node: Node) =
-        let alreadyOpenValves =
-            node.openValves |> Seq.map (fun openValve -> openValve.valve.name) |> Set
+        let alreadyOpenValves = node.openValves
 
         let closedUsefulValves =
             Set.difference allValveNames (Set.union alreadyOpenValves brokenValves)
 
-        if node.minutesLeft = 0 then
+        if node.minutesLeft <= 0 then
             []
         elif node.minutesLeft > 0 && closedUsefulValves.Count = 0 then
             // Stay at current valve and do nothing.
@@ -122,44 +171,53 @@ let part1 =
                     yield!
                         [ { node with
                               minutesLeft = node.minutesLeft - 1
-                              openValves =
-                                  [ { valve = node.currentlyAtValve
-                                      openAtMinutesLeft = node.minutesLeft - 1 } ]
-                                  |> List.append node.openValves
+                              openValves = node.openValves |> Set.add node.currentlyAtValve.name
+                              pressureReleased =
+                                  node.pressureReleased
+                                  + ((node.minutesLeft - 1) * node.currentlyAtValve.flowRate)
                               traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath } ]
                 // Travel to other valves
                 yield!
                     node.currentlyAtValve.leadsTo
-                    |> Seq.map getValve
-                    |> Seq.map (fun destinationValve ->
-                        { currentlyAtValve = destinationValve
-                          minutesLeft = node.minutesLeft - 1
-                          openValves = node.openValves
-                          traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath })
+                    |> Seq.filter (fun v -> node.minutesLeft >= v.costInMinutes)
+                    |> Seq.map (fun v -> (getValve v.name, v.costInMinutes))
+                    |> Seq.map (fun (destinationValve, travelCostInMinutes) ->
+                        { node with
+                            currentlyAtValve = destinationValve
+                            minutesLeft = node.minutesLeft - travelCostInMinutes
+                            traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath })
             }
             |> Seq.toList
 
-    let pressureReleased (node: Node) =
-        let pressureReleasedSoFar =
-            node.openValves
-            |> Seq.sumBy (fun openValve -> openValve.valve.flowRate * openValve.openAtMinutesLeft)
+    let heuristicFn (node: Node) =
 
-        pressureReleasedSoFar
+        let rec potential node depth =
+            if depth = 0 then
+                node.pressureReleased
+            else
+                let neighbors = getNeighbors node
 
-    let heuristicFn (node: Node) = 0
+                let result =
+                    if neighbors.Length > 0 then
+                        neighbors |> Seq.map (fun n -> potential n (depth - 1)) |> Seq.max
+                    else
+                        node.pressureReleased
 
-    let costFn (fromNode: Node) (toNode: Node) =
-        let fromReleased = pressureReleased fromNode
-        let toReleased = pressureReleased toNode
-        let result = toReleased - fromReleased
-        -result
+                result
+
+        let potentialValue = potential node 12
+
+        -(potentialValue)
+
+    let costFn (fromNode: Node) (toNode: Node) = 0
 
     let timeLimit = 30
 
     let startNode =
         { currentlyAtValve = getValve "AA"
           minutesLeft = timeLimit
-          openValves = []
+          openValves = Set.empty
+          pressureReleased = 0
           traveledPath = [] }
 
     let currentFn (node: Node) = ()
@@ -170,7 +228,7 @@ let part1 =
     let bestPath =
         aStar getNeighbors heuristicFn costFn startNode goalReachedFn currentFn
 
-    let pressureReleased = pressureReleased bestPath.Value.Head.Value
-    0
+    let pressureReleased = bestPath.Value.Head.Value.pressureReleased
+    pressureReleased
 
 let part2 = 0
