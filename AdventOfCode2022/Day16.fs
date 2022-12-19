05d5386ac10956fc3647841808ac7f4cbc2d962a
stil <p@ryciuk.com>
2022-12-20 00:44:07 +0100

    Solve day 16

--- AdventOfCode2022/Day16.fs
+++ AdventOfCode2022/Day16.fs
@@ -13,8 +13,7 @@ type Node =
     { minutesLeft: int
       valveLabel: string
       brokenOrOpenValves: string Set
-      pressureReleased: int
-      cameFrom: string list }
+      pressureReleased: int }
 
 let rec flattenDestination2
     (visited: string Set)
@@ -92,7 +91,7 @@ let optimizedValveDefinitions =
                 valve.leadsTo
                 |> Set.filter (fun destValve ->
                     let valve = getValve destValve.name
-                    valve.flowRate > 0) })
+                    valve.flowRate > 0 || valve.name = "AA") })
     |> Seq.toList
 
 let valveDefinitionsDict =
@@ -107,8 +106,10 @@ let part1 =
     let getNeighbors (node: Node) =
         let closedUsefulValves = Set.difference allValveNames node.brokenOrOpenValves
 
-        if closedUsefulValves.Count = 0 || node.minutesLeft <= 0 then
+        if node.minutesLeft <= 0 then
             Seq.empty
+        elif closedUsefulValves.Count = 0 && node.minutesLeft > 0 then
+            [ { node with minutesLeft = 0 } ]
         else
             let canBeOpened = closedUsefulValves |> Set.contains node.valveLabel
             let valve = getValve node.valveLabel
@@ -122,8 +123,7 @@ let part1 =
                         { node with
                             minutesLeft = node.minutesLeft - 1
                             brokenOrOpenValves = node.brokenOrOpenValves |> Set.add node.valveLabel
-                            pressureReleased = node.pressureReleased + (minutesUsed * valve.flowRate)
-                            cameFrom = node.valveLabel :: node.cameFrom }
+                            pressureReleased = node.pressureReleased + (minutesUsed * valve.flowRate) }
 
 
                 // Travel to other valves
@@ -132,8 +132,7 @@ let part1 =
                     |> Seq.map (fun dest ->
                         { node with
                             valveLabel = dest.name
-                            minutesLeft = node.minutesLeft - dest.costInMinutes
-                            cameFrom = node.valveLabel :: node.cameFrom })
+                            minutesLeft = node.minutesLeft - dest.costInMinutes })
 
                 yield! nextDestinations
             }
@@ -150,8 +149,7 @@ let part1 =
         { valveLabel = "AA"
           minutesLeft = timeLimit
           brokenOrOpenValves = brokenValves
-          pressureReleased = 0
-          cameFrom = [] }
+          pressureReleased = 0 }
 
     let toVisit = Stack<Node>()
     toVisit.Push(startNode)
@@ -174,8 +172,8 @@ let part1 =
                 |> Seq.toList
 
             let timer =
-                current.minutesLeft
-                |> Seq.unfold (fun state -> if state > 2 then Some(state, state - 2) else None)
+                (current.minutesLeft - 2)
+                |> Seq.unfold (fun state -> if state > 1 then Some(state, state - 2) else None)
 
             let potential =
                 timer
@@ -193,4 +191,251 @@ let part1 =
     let result = maxVal
     result
 
-let part2 = 0
+type DoubleNode =
+    { brokenOrOpenValves: string Set
+      pressureReleased: int
+      humanMinutesLeft: int
+      humanValveLabel: string
+      elephantMinutesLeft: int
+      elephantValveLabel: string }
+
+let part2 =
+    let allValveNames =
+        optimizedValveDefinitions |> Seq.map (fun valve -> valve.name) |> Set
+
+    let getNeighbors (node: DoubleNode) =
+        let closedUsefulValves =
+            Set.difference allValveNames (Set.union node.brokenOrOpenValves (Set.singleton "AA"))
+
+        if node.humanMinutesLeft <= 0 && node.elephantMinutesLeft <= 0 then
+            Seq.empty
+        elif
+            closedUsefulValves.Count = 0
+            && (node.humanMinutesLeft > 0 || node.elephantMinutesLeft > 0)
+        then
+            [ { node with
+                  humanMinutesLeft = 0
+                  elephantMinutesLeft = 0 } ]
+        else
+            seq {
+                let humanValve = getValve node.humanValveLabel
+
+                let humanCanBeOpened =
+                    humanValve.flowRate > 0
+                    && node.humanMinutesLeft > 0
+                    && (closedUsefulValves |> Set.contains node.humanValveLabel)
+
+                let elephantValve = getValve node.elephantValveLabel
+
+                let elephantCanBeOpened =
+                    elephantValve.flowRate > 0
+                    && node.elephantMinutesLeft > 0
+                    && (closedUsefulValves |> Set.contains node.elephantValveLabel)
+
+
+                let humanOpenActions = if humanCanBeOpened then [ true; false ] else [ false ]
+                let elephantOpenActions = if elephantCanBeOpened then [ true; false ] else [ false ]
+
+                let actions =
+                    seq {
+                        for humanOpenAction in humanOpenActions do
+                            for elephantOpenAction in elephantOpenActions do
+                                yield (humanOpenAction, elephantOpenAction)
+                    }
+                    |> Set
+
+                let actionsFiltered =
+                    if (node.humanValveLabel = node.elephantValveLabel) then
+                        actions |> Set.remove (true, true)
+                    else
+                        actions
+
+
+                for (humanOpenAction, elephantOpenAction) in actionsFiltered do
+                    if humanOpenAction && elephantOpenAction then
+                        yield
+                            { node with
+                                humanMinutesLeft = node.humanMinutesLeft - 1
+                                elephantMinutesLeft = node.elephantMinutesLeft - 1
+                                brokenOrOpenValves =
+                                    node.brokenOrOpenValves
+                                    |> Set.add node.humanValveLabel
+                                    |> Set.add node.elephantValveLabel
+                                pressureReleased =
+                                    node.pressureReleased
+                                    + ((node.humanMinutesLeft - 1) * humanValve.flowRate)
+                                    + ((node.elephantMinutesLeft - 1) * elephantValve.flowRate) }
+                    elif humanOpenAction && not (elephantOpenAction) then
+                        yield
+                            { node with
+                                humanMinutesLeft = node.humanMinutesLeft - 1
+                                brokenOrOpenValves = node.brokenOrOpenValves |> Set.add node.humanValveLabel
+                                pressureReleased =
+                                    node.pressureReleased + ((node.humanMinutesLeft - 1) * humanValve.flowRate) }
+                    elif not (humanOpenAction) && elephantOpenAction then
+                        yield
+                            { node with
+                                elephantMinutesLeft = node.elephantMinutesLeft - 1
+                                brokenOrOpenValves = node.brokenOrOpenValves |> Set.add node.elephantValveLabel
+                                pressureReleased =
+                                    node.pressureReleased
+                                    + ((node.elephantMinutesLeft - 1) * elephantValve.flowRate) }
+
+
+
+                for humanDest in
+                    (if node.humanMinutesLeft > 0 then
+                         humanValve.leadsTo
+                     else
+                         ([ { name = node.humanValveLabel
+                              costInMinutes = 0 } ]
+                          |> Set)) do
+                    for elephantDest in
+                        (if node.elephantMinutesLeft > 0 then
+                             elephantValve.leadsTo
+                         else
+                             [ { name = node.elephantValveLabel
+                                 costInMinutes = 0 } ]
+                             |> Set) do
+                        yield
+                            { node with
+                                humanValveLabel = humanDest.name
+                                humanMinutesLeft = node.humanMinutesLeft - humanDest.costInMinutes
+                                elephantValveLabel = elephantDest.name
+                                elephantMinutesLeft = node.elephantMinutesLeft - elephantDest.costInMinutes }
+
+
+            }
+
+    let timeLimit = 26
+
+    let brokenValves =
+        optimizedValveDefinitions
+        |> Seq.filter (fun valve -> valve.flowRate = 0)
+        |> Seq.map (fun valve -> valve.name)
+        |> Set
+
+    let startNode =
+        { brokenOrOpenValves = Set.empty
+          pressureReleased = 0
+          humanMinutesLeft = timeLimit
+          humanValveLabel = "AA"
+          elephantMinutesLeft = timeLimit
+          elephantValveLabel = "AA" }
+
+    let toVisit = Stack<DoubleNode>()
+    toVisit.Push(startNode)
+    let mutable maxVal = 0
+    let mutable goal = None
+
+    let mutable index = 0
+
+    let valveDefinitionsDict =
+        valveDefinitionsTangled |> Seq.map (fun vd -> (vd.name, vd)) |> dict
+
+    // let humanValves =
+    //     Queue<string * string option>(
+    //         [ ("AA", None)
+    //           ("II", None)
+    //           ("JJ", None)
+    //           ("JJ", Some "JJ")
+    //           ("II", None)
+    //           ("AA", None)
+    //           ("BB", None)
+    //           ("BB", Some "BB")
+    //           ("CC", None)
+    //           ("CC", Some "CC") ]
+    //         |> Seq.filter (fun (v, x) -> valveDefinitionsDict[v].flowRate > 0 || v = "AA")
+    //     )
+    //
+    // let elephantValves =
+    //     Queue<string * string option>(
+    //         [ ("AA", None)
+    //           ("DD", None)
+    //           ("DD", Some "DD")
+    //           ("EE", None)
+    //           ("FF", None)
+    //           ("GG", None)
+    //           ("HH", None)
+    //           ("HH", Some "HH")
+    //           ("GG", None)
+    //           ("FF", None)
+    //           ("EE", None)
+    //           ("EE", Some "EE") ]
+    //         |> Seq.filter (fun (v, x) -> valveDefinitionsDict[v].flowRate > 0 || v = "AA")
+    //     )
+
+    // let testHuman = humanValves.Dequeue()
+    // let testEleph = elephantValves.Dequeue()
+
+
+    while toVisit.Count > 0 do
+        let current = toVisit.Pop()
+
+        if current.humanMinutesLeft <= 0 && current.elephantMinutesLeft <= 0 then
+            if current.pressureReleased > maxVal then
+                maxVal <- max maxVal current.pressureReleased
+                goal <- Some current
+        else
+            let neighbors = getNeighbors current
+
+            let closedUsefulValvesTotalFlow =
+                Set.difference allValveNames current.brokenOrOpenValves
+                |> Seq.map getValve
+                |> Seq.map (fun v -> v.flowRate)
+                |> Seq.sortDescending
+                |> Seq.chunkBySize 1
+                |> Seq.toList
+            
+            let timer =
+                (min current.humanMinutesLeft current.elephantMinutesLeft)
+                |> Seq.unfold (fun state -> if state > 2 then Some(state, state - 2) else None)
+            
+            let potential =
+                timer
+                |> Seq.zip closedUsefulValvesTotalFlow
+                |> Seq.map (fun (a, b) -> b * (a |> Seq.sum))
+                |> Seq.sum
+            
+            if current.pressureReleased + potential < maxVal then
+                ()
+            else
+
+
+            // let dequeuedEleph, nextEleph = elephantValves.TryDequeue()
+            //
+            // let filterElephValve (node: DoubleNode) =
+            //     if dequeuedEleph then
+            //         node.elephantValveLabel = fst nextEleph
+            //         && ((snd nextEleph) |> Option.isNone
+            //             || node.brokenOrOpenValves |> Set.contains (snd nextEleph).Value)
+            //     else
+            //         true
+            //
+            // let dequeuedHuman, nextHuman = humanValves.TryDequeue()
+            //
+            // let filterHumanValve (node: DoubleNode) =
+            //     if dequeuedHuman then
+            //         node.humanValveLabel = fst nextHuman
+            //         && ((snd nextHuman) |> Option.isNone
+            //             || node.brokenOrOpenValves |> Set.contains (snd nextHuman).Value)
+            //     else
+            //         true
+            //
+            //
+            // let filteredNeighbors =
+            //     neighbors
+            //     |> Seq.filter filterHumanValve
+            //     |> Seq.filter filterElephValve
+            //     |> Seq.toList
+            //
+            // index <- index + 1
+
+
+
+                for next in neighbors do
+                    toVisit.Push(next)
+
+
+    let result = maxVal
+    result
