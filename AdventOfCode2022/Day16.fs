294c40e3f9c1754b0b1e9303b8bcf2641aa0498f
stil <p@ryciuk.com>
2022-12-19 01:51:32 +0100

    Finally solved after fixing goal function from < to <=

--- AdventOfCode2022/Day16.fs
+++ AdventOfCode2022/Day16.fs
@@ -9,7 +9,31 @@ type ValveDefinition =
       flowRate: int
       leadsTo: ValveDestination Set }
 
-let rec valveDefinitions () =
+type Node =
+    { minutesLeft: int
+      valveLabel: string
+      brokenOrOpenValves: string Set
+      pressureReleased: int
+      cameFrom: string list }
+
+let rec flattenDestination2
+    (visited: string Set)
+    (leadsToFn: string -> ValveDestination Set)
+    (destination: ValveDestination)
+    =
+    let result =
+        (leadsToFn destination.name)
+        |> Seq.filter (fun innerDest -> not (visited |> Set.contains innerDest.name))
+        |> Seq.map (fun innerDest ->
+            ({ innerDest with costInMinutes = innerDest.costInMinutes + destination.costInMinutes }
+             |> flattenDestination2 (visited |> Set.add innerDest.name) leadsToFn
+             |> Seq.toList))
+        |> Seq.collect id
+        |> Seq.toList
+
+    destination :: result
+
+let valveDefinitionsTangled =
     let definitions =
         Helpers.readInput 16
         |> Seq.map (fun line ->
@@ -34,201 +58,139 @@ let rec valveDefinitions () =
         |> Seq.toList
 
     let valveDefinitionsDict = definitions |> Seq.map (fun vd -> (vd.name, vd)) |> dict
-
     let getValve valveName = valveDefinitionsDict[valveName]
 
-    let rec flattenDestinations depth (destinations: ValveDestination seq) =
-        if depth = 11 then
-            []
-        else
-            let leadsTo =
-                destinations
-                |> Seq.map (fun dest ->
-                    let destValve = getValve dest.name
+    let leadsToFn valveLabel =
+        let valve = getValve valveLabel
+        valve.leadsTo
 
-                    if destValve.flowRate = 0 then
-                        destValve.leadsTo |> flattenDestinations (depth + 1)
-                    else
-                        [ { dest with costInMinutes = depth } ])
-                |> Seq.collect id
-                |> Seq.toList
-
-            leadsTo
-
-    let flattenValve (valve: ValveDefinition) =
+    let flattenValve2 (valve: ValveDefinition) =
         { valve with
             leadsTo =
                 valve.leadsTo
-                |> flattenDestinations 1
-                |> Set
-                |> Seq.sortBy (fun dest -> (dest.name, dest.costInMinutes))
-                |> Seq.distinctBy (fun dest -> dest.name)
-                |> Seq.filter (fun dest -> dest.name <> valve.name)
+                |> Seq.map (flattenDestination2 (Set [ valve.name ]) leadsToFn)
+                |> Seq.collect id
+                |> Seq.sortBy (fun (x: ValveDestination) -> (x.name, x.costInMinutes))
+                |> Seq.distinctBy (fun (x: ValveDestination) -> x.name)
                 |> Set }
 
-    let result =
-        definitions
-        |> List.map flattenValve
-        |> Seq.filter (fun def -> def.name = "AA" || def.flowRate > 0)
-        |> Seq.toList
-
+    let result = definitions |> Seq.map flattenValve2 |> Seq.toList
 
     result
 
-let aStar neighborsFn heuristicFn costFn (start: 'a) (goalReachedFn: 'a -> bool) (currentFn: 'a -> unit) =
-    let frontier = PriorityQueue<'a, int>()
-    let cameFrom = Dictionary<'a, 'a option>()
-    let costSoFar = Dictionary<'a, int>()
-
-    frontier.Enqueue(start, 0)
-    cameFrom[start] <- None
-    costSoFar[start] <- 0
-
-    let mutable goal = None
-
-    while Option.isNone goal && frontier.Count > 0 do
-        let current = frontier.Dequeue()
-        currentFn current
-
-        if goalReachedFn current then
-            goal <- Some current
-        else
-            neighborsFn current
-            |> Seq.iter (fun next ->
-                let newCost = costSoFar[current] + costFn current next
-
-                if not (costSoFar.ContainsKey(next)) || newCost < costSoFar[next] then
-                    costSoFar[next] <- newCost
-                    let priority = newCost + heuristicFn next
-                    frontier.Enqueue(next, priority)
-                    cameFrom[next] <- Some(current)
-                    ()
-
-                ())
-
-
-    if Option.isNone goal then
-        None
-    else
-        let shortestPath =
-            goal
-            |> Seq.unfold (fun somePoint ->
-                match somePoint with
-                | Some point ->
-                    let found, prevPoint = cameFrom.TryGetValue(point)
-                    Some(somePoint, prevPoint)
-                | None -> None)
-            |> Seq.toList
-
-        if shortestPath |> Seq.last = Some(start) then
-            Some(shortestPath)
-        else
-            None
-
-
-type Node =
-    { minutesLeft: int
-      currentlyAtValve: ValveDefinition
-      openValves: string Set
-      pressureReleased: int
-      traveledPath: string list }
-
-let part1 =
-    let valveDefinitions = valveDefinitions ()
-
+let optimizedValveDefinitions =
     let valveDefinitionsDict =
-        valveDefinitions |> Seq.map (fun vd -> (vd.name, vd)) |> dict
+        valveDefinitionsTangled |> Seq.map (fun vd -> (vd.name, vd)) |> dict
 
     let getValve valveName = valveDefinitionsDict[valveName]
 
-    let allValveNames = valveDefinitions |> Seq.map (fun valve -> valve.name) |> Set
+    valveDefinitionsTangled
+    |> Seq.filter (fun def -> def.flowRate > 0 || def.name = "AA")
+    |> Seq.map (fun valve ->
+        { valve with
+            leadsTo =
+                valve.leadsTo
+                |> Set.filter (fun destValve ->
+                    let valve = getValve destValve.name
+                    valve.flowRate > 0) })
+    |> Seq.toList
 
-    let brokenValves =
-        valveDefinitions
-        |> Seq.filter (fun valve -> valve.flowRate = 0)
-        |> Seq.map (fun valve -> valve.name)
-        |> Set
+let valveDefinitionsDict =
+    optimizedValveDefinitions |> Seq.map (fun vd -> (vd.name, vd)) |> dict
+
+let getValve valveName = valveDefinitionsDict[valveName]
+
+let part1 =
+    let allValveNames =
+        optimizedValveDefinitions |> Seq.map (fun valve -> valve.name) |> Set
 
     let getNeighbors (node: Node) =
-        let alreadyOpenValves = node.openValves
+        let closedUsefulValves = Set.difference allValveNames node.brokenOrOpenValves
 
-        let closedUsefulValves =
-            Set.difference allValveNames (Set.union alreadyOpenValves brokenValves)
-
-        if node.minutesLeft <= 0 then
-            []
-        elif node.minutesLeft > 0 && closedUsefulValves.Count = 0 then
-            // Stay at current valve and do nothing.
-            [ { node with
-                  minutesLeft = node.minutesLeft - 1
-                  traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath } ]
+        if closedUsefulValves.Count = 0 || node.minutesLeft <= 0 then
+            Seq.empty
         else
-            let canBeOpened = closedUsefulValves |> Set.contains node.currentlyAtValve.name
+            let canBeOpened = closedUsefulValves |> Set.contains node.valveLabel
+            let valve = getValve node.valveLabel
 
             seq {
                 // Open current valve (if closed and useful)
                 if canBeOpened then
-                    yield!
-                        [ { node with
-                              minutesLeft = node.minutesLeft - 1
-                              openValves = node.openValves |> Set.add node.currentlyAtValve.name
-                              pressureReleased =
-                                  node.pressureReleased
-                                  + ((node.minutesLeft - 1) * node.currentlyAtValve.flowRate)
-                              traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath } ]
-                // Travel to other valves
-                yield!
-                    node.currentlyAtValve.leadsTo
-                    |> Seq.filter (fun v -> node.minutesLeft >= v.costInMinutes)
-                    |> Seq.map (fun v -> (getValve v.name, v.costInMinutes))
-                    |> Seq.map (fun (destinationValve, travelCostInMinutes) ->
+                    let minutesUsed = node.minutesLeft - 1
+
+                    yield
                         { node with
-                            currentlyAtValve = destinationValve
-                            minutesLeft = node.minutesLeft - travelCostInMinutes
-                            traveledPath = [ node.currentlyAtValve.name ] |> List.append node.traveledPath })
+                            minutesLeft = node.minutesLeft - 1
+                            brokenOrOpenValves = node.brokenOrOpenValves |> Set.add node.valveLabel
+                            pressureReleased = node.pressureReleased + (minutesUsed * valve.flowRate)
+                            cameFrom = node.valveLabel :: node.cameFrom }
+
+
+                // Travel to other valves
+                let nextDestinations =
+                    valve.leadsTo
+                    |> Seq.map (fun dest ->
+                        { node with
+                            valveLabel = dest.name
+                            minutesLeft = node.minutesLeft - dest.costInMinutes
+                            cameFrom = node.valveLabel :: node.cameFrom })
+
+                yield! nextDestinations
             }
-            |> Seq.toList
-
-    let heuristicFn (node: Node) =
-
-        let rec potential node depth =
-            if depth = 0 then
-                node.pressureReleased
-            else
-                let neighbors = getNeighbors node
-
-                let result =
-                    if neighbors.Length > 0 then
-                        neighbors |> Seq.map (fun n -> potential n (depth - 1)) |> Seq.max
-                    else
-                        node.pressureReleased
-
-                result
-
-        let potentialValue = potential node 12
-
-        -(potentialValue)
-
-    let costFn (fromNode: Node) (toNode: Node) = 0
 
     let timeLimit = 30
 
+    let brokenValves =
+        optimizedValveDefinitions
+        |> Seq.filter (fun valve -> valve.flowRate = 0)
+        |> Seq.map (fun valve -> valve.name)
+        |> Set
+
     let startNode =
-        { currentlyAtValve = getValve "AA"
+        { valveLabel = "AA"
           minutesLeft = timeLimit
-          openValves = Set.empty
+          brokenOrOpenValves = brokenValves
           pressureReleased = 0
-          traveledPath = [] }
+          cameFrom = [] }
 
-    let currentFn (node: Node) = ()
-    //printfn "Current node: %s" node.currentlyAtValve.name
+    let toVisit = Stack<Node>()
+    toVisit.Push(startNode)
+    let mutable maxVal = 0
+    let mutable goal = None
 
-    let goalReachedFn (node: Node) = node.minutesLeft = 0
+    while toVisit.Count > 0 do
+        let current = toVisit.Pop()
 
-    let bestPath =
-        aStar getNeighbors heuristicFn costFn startNode goalReachedFn currentFn
+        if current.minutesLeft <= 0 then
+            if current.pressureReleased > maxVal then
+                maxVal <- max maxVal current.pressureReleased
+                goal <- Some current
+        else
+            let closedUsefulValvesTotalFlow =
+                Set.difference allValveNames current.brokenOrOpenValves
+                |> Seq.map getValve
+                |> Seq.map (fun v -> v.flowRate)
+                |> Seq.sortDescending
+                |> Seq.toList
 
-    let pressureReleased = bestPath.Value.Head.Value.pressureReleased
-    pressureReleased
+            let timer =
+                current.minutesLeft
+                |> Seq.unfold (fun state -> if state > 2 then Some(state, state - 2) else None)
+
+            let potential =
+                timer
+                |> Seq.zip closedUsefulValvesTotalFlow
+                |> Seq.map (fun (a, b) -> a * b)
+                |> Seq.sum
+
+            if current.pressureReleased + potential < maxVal then
+                ()
+            else
+                for next in (getNeighbors current) do
+                    toVisit.Push(next)
+
+
+    let result = maxVal
+    result
 
 let part2 = 0
