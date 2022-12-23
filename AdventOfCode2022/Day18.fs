dc785e4e661b47c0a78c59020cbe6cdf4db3a1bb
stil <p@ryciuk.com>
2022-12-23 23:17:08 +0100

    Solve day 18 part 1

--- AdventOfCode2022/Day18.fs
+++ AdventOfCode2022/Day18.fs
@@ -1,4 +1,60 @@
 module Day18
 
-let part1 = 0
+type Cube = int * int * int
+
+type Side =
+    | Front
+    | Back
+    | Left
+    | Right
+    | Up
+    | Down
+
+let part1 =
+    let input =
+        Helpers.readInput 18
+        |> Seq.map (fun line -> line.Split(',') |> Seq.map int |> Seq.toList)
+        |> Seq.map (fun parts -> (parts[0], parts[1], parts[2]))
+        |> Seq.toArray
+
+    let minX = input |> Seq.map (fun (x, y, z) -> x) |> Seq.min
+    let minY = input |> Seq.map (fun (x, y, z) -> y) |> Seq.min
+    let minZ = input |> Seq.map (fun (x, y, z) -> z) |> Seq.min
+    let maxX = input |> Seq.map (fun (x, y, z) -> x) |> Seq.max
+    let maxY = input |> Seq.map (fun (x, y, z) -> y) |> Seq.max
+    let maxZ = input |> Seq.map (fun (x, y, z) -> z) |> Seq.max
+
+    // Left handed cartesian coordinates.
+    let getCubeOnSide (x, y, z) side =
+        match side with
+        | Front -> (x, y, z - 1)
+        | Back -> (x, y, z + 1)
+        | Left -> (x - 1, y, z)
+        | Right -> (x + 1, y, z)
+        | Up -> (x, y + 1, z)
+        | Down -> (x, y - 1, z)
+
+    let grid = Array3D.create (maxX + 1) (maxY + 1) (maxZ + 1) false
+
+    input |> Seq.iter (fun (x, y, z) -> Array3D.set grid x y z true)
+
+    let isTaken (x, y, z) =
+        x < minX
+        || y < minY
+        || z < minZ
+        || x > maxX
+        || y > maxY
+        || z > maxZ
+        || not (Array3D.get grid x y z)
+
+    let sides = [ Front; Back; Left; Right; Up; Down ]
+
+    let result =
+        input
+        |> Seq.map (fun (x, y, z) -> sides |> Seq.map (getCubeOnSide (x, y, z)) |> Seq.filter isTaken)
+        |> Seq.sumBy (fun uncoveredSides -> uncoveredSides |> Seq.length)
+
+    result
+
+
 let part2 = 0
--- AdventOfCode2022/Program.fs
+++ AdventOfCode2022/Program.fs
@@ -1,2 +1,2 @@
-﻿printfn $"Part 1: {Day17.part1}"
-printfn $"Part 2: {Day17.part2}"
+﻿printfn $"Part 1: {Day18.part1}"
+printfn $"Part 2: {Day18.part2}"
