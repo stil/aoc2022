ad2c8be47db2144fcd1e16c40272c6331fd6d766
stil <p@ryciuk.com>
2022-12-27 01:44:59 +0100

    Solve part2 example

--- AdventOfCode2022/Day22.fs
+++ AdventOfCode2022/Day22.fs
@@ -1,5 +1,7 @@
 module Day22
 
+open System.Diagnostics
+
 let inputParts = (Helpers.readInputFull 22).Split("\r\n\r\n")
 let map = inputParts[ 0 ].Split("\r\n")
 
@@ -22,7 +24,7 @@ let instructions =
         | _ -> Move(int m.Groups[1].Value))
     |> Seq.toList
 
-type Facing =
+type Direction =
     | Right
     | Down
     | Left
@@ -31,21 +33,31 @@ type Facing =
 type State =
     { Row: int // 0-based
       Column: int // 0-based
-      Facing: Facing }
+      Facing: Direction }
+
+let password state =
+    [ (state.Row + 1) * 1000
+      (state.Column + 1) * 4
+      match state.Facing with
+      | Right -> 0
+      | Down -> 1
+      | Left -> 2
+      | Up -> 3 ]
+    |> Seq.sum
+
+let makeStep row column facing =
+    match facing with
+    | Right -> (row, column + 1)
+    | Down -> (row + 1, column)
+    | Left -> (row, column - 1)
+    | Up -> (row - 1, column)
+
+let initialState =
+    { Row = 0
+      Column = map[0] |> Seq.findIndex (fun c -> c = '.')
+      Facing = Right }
 
 let part1 =
-    let initialState =
-        { Row = 0
-          Column = map[0] |> Seq.findIndex (fun c -> c = '.')
-          Facing = Right }
-
-    let makeStep row column facing =
-        match facing with
-        | Right -> (row, column + 1)
-        | Down -> (row + 1, column)
-        | Left -> (row, column - 1)
-        | Up -> (row - 1, column)
-
     let measureLine line =
         let min = line |> Seq.takeWhile (fun c -> c = ' ') |> Seq.length
 
@@ -57,7 +69,6 @@ let part1 =
         (min, max)
 
     let applyWrap facing row column =
-
         if facing = Up || facing = Down then
             let vertical =
                 [ 0 .. (map.Length - 1) ]
@@ -65,8 +76,7 @@ let part1 =
                     if rowIndex >= map.Length then ' '
                     elif rowIndex < 0 then ' '
                     elif column >= map[rowIndex].Length then ' '
-                    else map[rowIndex][column]
-                    )
+                    else map[rowIndex][column])
                 |> Seq.toList
 
             let minRowIncl, maxRowExcl = measureLine vertical
@@ -111,7 +121,7 @@ let part1 =
                 Seq.init steps id
                 |> Seq.fold
                     (fun state _ ->
-                        let (nextRow, nextColumn) =
+                        let nextRow, nextColumn =
                             makeStep state.Row state.Column state.Facing ||> applyWrap state.Facing
 
                         if map[nextRow][nextColumn] = '#' then
@@ -126,23 +136,206 @@ let part1 =
 
             finalState
 
-
-
-
     let finalState = instructions |> Seq.fold advanceInstruction initialState
-
-    let password state =
-        [ (state.Row + 1) * 1000
-          (state.Column + 1) * 4
-          match state.Facing with
-          | Right -> 0
-          | Down -> 1
-          | Left -> 2
-          | Up -> 3 ]
-        |> Seq.sum
-
     let result = password finalState
     result
 
+type Face =
+    | Front
+    | Left
+    | Right
+    | Up
+    | Down
+    | Back
 
-let part2 = 0
+let part2 =
+    let measureLine line =
+        let min = line |> Seq.takeWhile (fun c -> c = ' ') |> Seq.length
+
+        let rowLength =
+            line |> Seq.skip min |> Seq.takeWhile (fun c -> c <> ' ') |> Seq.length
+
+        let max = min + rowLength
+
+        (min, max)
+
+    let size = 4
+
+    let faces =
+        [ [ None; None; Some(Up) ]
+          [ Some(Back); Some(Left); Some(Front) ]
+          [ None; None; Some(Down); Some(Right) ] ]
+        |> Seq.mapi (fun rowI row ->
+            row
+            |> Seq.mapi (fun colI face ->
+                match face with
+                | Some face -> Some((face, (rowI * size, colI * size)))
+                | None -> None))
+        |> Seq.collect id
+        |> Seq.choose id
+        |> Seq.toList
+
+
+    let wrapRules =
+        [ ((Face.Up, Direction.Up), (Face.Back, Direction.Down))
+          ((Face.Up, Direction.Right), (Face.Right, Direction.Left))
+          ((Face.Up, Direction.Left), (Face.Left, Direction.Down))
+          ((Face.Up, Direction.Down), (Face.Front, Direction.Down))
+          // --
+          ((Face.Front, Direction.Up), (Face.Up, Direction.Up))
+          ((Face.Front, Direction.Right), (Face.Right, Direction.Down))
+          ((Face.Front, Direction.Left), (Face.Left, Direction.Left))
+          ((Face.Front, Direction.Down), (Face.Down, Direction.Down))
+          // --
+          ((Face.Right, Direction.Up), (Face.Front, Direction.Left))
+          ((Face.Right, Direction.Right), (Face.Up, Direction.Left))
+          ((Face.Right, Direction.Left), (Face.Down, Direction.Left))
+          ((Face.Right, Direction.Down), (Face.Back, Direction.Right))
+          // --
+          ((Face.Left, Direction.Up), (Face.Up, Direction.Right))
+          ((Face.Left, Direction.Right), (Face.Front, Direction.Right))
+          ((Face.Left, Direction.Left), (Face.Back, Direction.Left))
+          ((Face.Left, Direction.Down), (Face.Down, Direction.Right))
+          // --
+          ((Face.Back, Direction.Up), (Face.Up, Direction.Down))
+          ((Face.Back, Direction.Right), (Face.Left, Direction.Right))
+          ((Face.Back, Direction.Left), (Face.Right, Direction.Up))
+          ((Face.Back, Direction.Down), (Face.Down, Direction.Up))
+          // --
+          ((Face.Down, Direction.Up), (Face.Front, Direction.Up))
+          ((Face.Down, Direction.Right), (Face.Right, Direction.Right))
+          ((Face.Down, Direction.Left), (Face.Left, Direction.Up))
+          ((Face.Down, Direction.Down), (Face.Back, Direction.Up)) ]
+
+    let makeStep row column (facing: Direction) =
+        let currentFace =
+            faces
+            |> Seq.find (fun (faceName, (r, c)) -> row >= r && row < r + size && column >= c && column < c + size)
+
+
+        let newRow, newColumn =
+            match facing with
+            | Direction.Right -> (row, column + 1)
+            | Direction.Down -> (row + 1, column)
+            | Direction.Left -> (row, column - 1)
+            | Direction.Up -> (row - 1, column)
+
+        let nextFace =
+            faces
+            |> Seq.tryFind (fun (faceName, (r, c)) ->
+                newRow >= r && newRow < r + size && newColumn >= c && newColumn < c + size)
+
+        let outOfBounds = Option.isNone nextFace || nextFace.Value <> currentFace
+
+        if outOfBounds then
+            let (nextFace, nextDirection) =
+                wrapRules
+                |> Seq.find (fun ((f, d), _) -> f = (fst currentFace) && d = facing)
+                |> snd
+
+            let _, (nextFaceBaseRow, nextFaceBaseCol) =
+                faces |> Seq.find (fun (faceName, (r, c)) -> faceName = nextFace)
+
+            let relPos =
+                match facing with
+                | Direction.Down -> column - (snd (snd currentFace))
+                | Direction.Up -> column - (snd (snd currentFace))
+                | Direction.Left -> row - (fst (snd currentFace))
+                | Direction.Right -> row - (fst (snd currentFace))
+
+            let lastPos = size - 1
+
+            let nextRow, nextColumn =
+                match (facing, nextDirection) with
+                | Direction.Down, Direction.Down -> (0, relPos)
+                | Direction.Up, Direction.Down -> (0, lastPos - relPos)
+                | Direction.Left, Direction.Down -> (0, relPos)
+                | Direction.Right, Direction.Down -> (0, lastPos - relPos)
+                // --
+                | Direction.Down, Direction.Up -> (lastPos, lastPos - relPos)
+                | Direction.Up, Direction.Up -> (lastPos, relPos)
+                | Direction.Left, Direction.Up -> (lastPos, lastPos - relPos)
+                | Direction.Right, Direction.Up -> (lastPos,  relPos)
+                // --
+                | Direction.Down, Direction.Left -> (relPos, lastPos)
+                | Direction.Up, Direction.Left -> (lastPos - relPos, lastPos)
+                | Direction.Left, Direction.Left -> (relPos, lastPos)
+                | Direction.Right, Direction.Left -> (lastPos - relPos, lastPos)
+                // --
+                | Direction.Down, Direction.Right -> (lastPos - relPos, 0)
+                | Direction.Up, Direction.Right -> (relPos, 0)
+                | Direction.Left, Direction.Right -> (lastPos - relPos, 0)
+                | Direction.Right, Direction.Right -> (relPos, 0)
+
+
+
+
+            let result =
+                (nextFaceBaseRow + nextRow, nextFaceBaseCol + nextColumn, nextDirection)
+
+            result
+        else
+            (newRow, newColumn, facing)
+
+
+
+    let advanceInstruction (state: State) (instr: Instruction) =
+        match instr with
+        | TurnR ->
+            { state with
+                Facing =
+                    match state.Facing with
+                    | Direction.Right -> Direction.Down
+                    | Direction.Down -> Direction.Left
+                    | Direction.Left -> Direction.Up
+                    | Direction.Up -> Direction.Right }
+        | TurnL ->
+            { state with
+                Facing =
+                    match state.Facing with
+                    | Direction.Right -> Direction.Up
+                    | Direction.Down -> Direction.Right
+                    | Direction.Left -> Direction.Down
+                    | Direction.Up -> Direction.Left }
+        | Move steps ->
+
+            let finalState =
+                Seq.init steps id
+                |> Seq.fold
+                    (fun state _ ->
+                        let mapCopy = map |> Seq.map (fun line -> line |> Seq.toList) |> Seq.toList
+
+                        let mapCopy2 =
+                            mapCopy
+                            |> Seq.map (fun line -> line |> Seq.toList)
+                            |> Seq.toList
+                            |> List.updateAt
+                                state.Row
+                                (mapCopy[state.Row] |> Seq.toList |> List.updateAt state.Column 'x')
+                            |> Seq.map System.String.Concat
+
+                        printfn "%s" (System.String.Join("\r\n", mapCopy2))
+                        printfn ""
+                        printfn ""
+                        printfn ""
+                        printfn ""
+
+
+                        let nextRow, nextColumn, nextFacing = makeStep state.Row state.Column state.Facing
+
+                        if map[nextRow][nextColumn] = '#' then
+                            state
+                        else
+                            { state with
+                                Column = nextColumn
+                                Row = nextRow
+                                Facing = nextFacing })
+                    state
+
+
+
+            finalState
+
+    let finalState = instructions |> Seq.fold advanceInstruction initialState
+    let result = password finalState
+    result
