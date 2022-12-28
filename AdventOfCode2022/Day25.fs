cdaf9e5062cdd1555d207af6486244a6d963d487
stil <p@ryciuk.com>
2022-12-28 18:43:46 +0100

    Solve day 25

--- AdventOfCode2022/Day25.fs
+++ AdventOfCode2022/Day25.fs
@@ -1,4 +1,48 @@
 module Day25
 
-let part1 = 0
+let part1 =
+    let snafuToDec snafu =
+        snafu
+        |> Seq.rev
+        |> Seq.mapi (fun i c -> (i, c))
+        |> Seq.sumBy (fun (i, c) ->
+            (pown 5L i)
+            * match c with
+              | '2' -> 2L
+              | '1' -> 1L
+              | '0' -> 0L
+              | '-' -> -1L
+              | '=' -> -2L
+              | _ -> failwith "Unexpected character.")
+
+    let resultDec = Helpers.readInput 25 |> Seq.map snafuToDec |> Seq.sum
+
+    let decToSnafu (dec: int64) =
+        let mutable number = dec
+        let mutable digits = []
+
+        while number > 0 do
+            let divisionResult = int64 (round (float number / 5.0))
+            let divisionRemainder = number - (divisionResult * 5L)
+            digits <- divisionRemainder :: digits
+            number <- divisionResult
+
+        let snafu =
+            digits
+            |> Seq.map (fun d ->
+                match int d with
+                | -2 -> '='
+                | -1 -> '-'
+                | 0 -> '0'
+                | 1 -> '1'
+                | 2 -> '2'
+                | _ -> failwith "Unexpected number.")
+
+        System.String.Concat(snafu)
+
+    let result = decToSnafu resultDec
+
+    result
+
+
 let part2 = 0
--- AdventOfCode2022/Program.fs
+++ AdventOfCode2022/Program.fs
@@ -1,2 +1,2 @@
-﻿printfn $"Part 1: {Day23.part1}"
-printfn $"Part 2: {Day23.part2}"
+﻿printfn $"Part 1: {Day25.part1}"
+printfn $"Part 2: {Day25.part2}"
